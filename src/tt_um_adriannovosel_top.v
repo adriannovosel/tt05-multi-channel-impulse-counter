ace944fd5c47a623d957a55cc5027cc2156db6d1
adriannovosel <134148571+adriannovosel@users.noreply.github.com>
2023-11-02 16:03:32 +0100

    Update tt_um_adriannovosel_top.v

--- src/tt_um_adriannovosel_top.v
+++ src/tt_um_adriannovosel_top.v
@@ -13,7 +13,7 @@ module tt_um_adriannovosel_top (
     input  wire       rst_n
 );
 
-    top top1(.reset(ui_in[0]), .clk(ui_in[1]), .ch1(ui_in[2]), .ch2(ui_in[3]), .ch3(ui_in[4]), .ch4(ui_in[5]),
+	top top1(.reset(ui_in[0]), .clk(clk), .ch1(ui_in[2]), .ch2(ui_in[3]), .ch3(ui_in[4]), .ch4(ui_in[5]),
 	     .ch5(uio_in[0]), .ch6(uio_in[1]), .ch7(uio_in[2]), .ch8(uio_in[3]), .RTC(ui_in[6]), 
              .serial_out(uo_out[0]), .ovf_global(uo_out[1]), .ovf_RTC_out(uo_out[2]), .a0_out(uo_out[3]), .a1_out(uo_out[4]), 
 	     .a2_out(uo_out[5]), .a3_out(uo_out[6]), .SL_out(uo_out[7]));
