a5e06e1b3cfc06d256b0ec6d9b48dc392c4c36ac
adriannovosel <134148571+adriannovosel@users.noreply.github.com>
2023-11-02 15:28:40 +0100

    Update tt_um_adriannovosel_top.v

--- src/tt_um_adriannovosel_top.v
+++ src/tt_um_adriannovosel_top.v
@@ -1,14 +1,17 @@
-//light up the leds according to a counter to cycle through every one
+`default_nettype none
 
-module tt_um_adriannovosel_top(input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
-                              );
+// just a stub to keep the Tiny Tapeout tools happy
+
+module tt_um_vhdl_seven_segment_seconds (
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
+);
 
     top top1(.reset(ui_in[0]), .clk(ui_in[1]), .ch1(ui_in[2]), .ch2(ui_in[3]), .ch3(ui_in[4]), .ch4(ui_in[5]),
 	     .ch5(uio_in[0]), .ch6(uio_in[1]), .ch7(uio_in[2]), .ch8(uio_in[3]), .RTC(ui_in[6]), 
